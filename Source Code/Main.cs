5d19f0fa7e512b76b775a8bc18ae49de8e63c281
FrizaSCPSL <161040188+FrizaSCPSL@users.noreply.github.com>
2024-03-07 15:55:58 +0300

    Add files via upload

--- Source Code/Main.cs
+++ Source Code/Main.cs
@@ -82,31 +82,31 @@ namespace Scp273
         {
             if (ev.Player.Role.Type == RoleTypeId.NtfCaptain)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             }
             if (ev.Player.Role.Type == RoleTypeId.NtfSergeant)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             }
             if (ev.Player.Role.Type == RoleTypeId.NtfPrivate)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             }
             if (ev.Player.Role.Type == RoleTypeId.ChaosMarauder)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             }
             if (ev.Player.Role.Type == RoleTypeId.ChaosRepressor)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             }
             if (ev.Player.Role.Type == RoleTypeId.ChaosRifleman)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             }
             if (ev.Player.Role.Type == RoleTypeId.ChaosConscript)
             {
-                ev.Player.CustomInfo = "Человек";
+                ev.Player.CustomInfo = null;
             } 
         }
 
@@ -114,7 +114,7 @@ namespace Scp273
         {
             if (ev.Player.CustomInfo == "Человек-Феникс.")
             {
-                ev.Player.CustomInfo = "Человек.";
+                ev.Player.CustomInfo = null;
             }
         }
         
@@ -125,13 +125,47 @@ namespace Scp273
                 Timing.CallDelayed(20f, delegate()
                 {
                     ev.Player.Role.Set(RoleTypeId.ClassD, RoleSpawnFlags.AssignInventory);
-                    ev.Player.CustomInfo = "Человек-Феникс.";
+                    ev.Player.CustomInfo = "Человек-Феникс. Жизнь 2";
                     ev.Player.RankColor = "orange";
                     ev.Player.RankName = "Scp-273";
                     ev.Player.MaxHealth = 170;
                     ev.Player.Health = 170;
                     ev.Player.ShowHint(Config.Hint2, 5f);
+                    ev.Player.Broadcast(7, "У вас осталось 3 жизни");
                 });
+            }
+            if (ev.Player.CustomInfo == "Человек-Феникс. Жизнь 2")
+            {
+                Timing.CallDelayed(20f, delegate()
+                {
+                    ev.Player.Role.Set(RoleTypeId.ClassD, RoleSpawnFlags.AssignInventory);
+                    ev.Player.CustomInfo = "Человек-Феникс. Жизнь 3";
+                    ev.Player.RankColor = "orange";
+                    ev.Player.RankName = "Scp-273";
+                    ev.Player.MaxHealth = 170;
+                    ev.Player.Health = 170;
+                    ev.Player.ShowHint(Config.Hint2, 5f);
+                    ev.Player.Broadcast(7, "У вас осталось 2 жизни");
+                });
+            }
+            if (ev.Player.CustomInfo == "Человек-Феникс. Жизнь 3")
+            {
+                Timing.CallDelayed(20f, delegate()
+                {
+                    ev.Player.Role.Set(RoleTypeId.ClassD, RoleSpawnFlags.AssignInventory);
+                    ev.Player.CustomInfo = "Человек-Феникс. Жизнь 4";
+                    ev.Player.RankColor = "orange";
+                    ev.Player.RankName = "Scp-273";
+                    ev.Player.MaxHealth = 170;
+                    ev.Player.Health = 170;
+                    ev.Player.ShowHint(Config.Hint2, 5f);
+                    ev.Player.Broadcast(7, "У вас осталась 1 жизнь");
+                });
+            }
+            if (ev.Player.CustomInfo == "Человек-Феникс. Жизнь 4")
+            {
+                ev.Player.CustomInfo = null;
+                ev.Player.Broadcast(7, "Вы мертвы!");
             }
         }
     }
